d235ac9701fbc03958d2873d80d2896e18f7689a
vifair22 <vifair22@gmail.com>
2024-02-22 17:30:01 -0700

    Create release.ps1

---
+++ release.ps1
@@ -0,0 +1,27 @@
+# Get the version number from version.txt
+$version = Get-Content -Path "./src/version.txt"
+
+# Directory paths
+$sourceDirectoryx64 = "./build/release/x64"
+$sourceDirectoryx86 = "./build/release/x86"
+$destinationDirectory = "./release"
+
+# Create the destination directory if it doesn't exist
+if (-not (Test-Path -Path $destinationDirectory)) {
+    New-Item -ItemType Directory -Path $destinationDirectory | Out-Null
+}
+
+# Copy .msi (x64) files and rename them
+Get-ChildItem -Path $sourceDirectoryx64 -Filter *.msi | ForEach-Object {
+    $newFileName = $_.BaseName + "-v$version-x64" + $_.Extension
+    $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $newFileName
+    Copy-Item -Path $_.FullName -Destination $destinationPath
+}
+# Copy .msi (x86) files and rename them
+Get-ChildItem -Path $sourceDirectoryx86 -Filter *.msi | ForEach-Object {
+    $newFileName = $_.BaseName + "-v$version-x86" + $_.Extension
+    $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $newFileName
+    Copy-Item -Path $_.FullName -Destination $destinationPath
+}
+
+Write-Host "Files copied and renamed successfully."
